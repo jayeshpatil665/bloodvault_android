c8ec73176930be8816697360776b76c507b9e9d4
Bajiraoo512 <jayeshpatil665@gmail.com>
2021-05-15 12:59:23 +0530

    forth_commit_ForthCommit

---
+++ app/src/main/java/in/specialsoft/bloodvault/DonorDetails/DonorDetails.java
@@ -0,0 +1,16 @@
+package in.specialsoft.bloodvault.DonorDetails;
+
+public class DonorDetails {
+
+    public static final String UserIDKey = "UserId";
+    public static final String UserNameKey = "UserName";
+    public static final String UserPhoneKey = "UserPhone";
+
+    public static final String UserAddressKey = "UserAddress";
+    public static final String UserCityKey = "UserCity";
+    public static final String UserBloodGroupKey = "UserBloodGroup";
+
+    public static final String UserGenderKey = "UserGender";
+    public static final String UserAvailableKey = "UserAvailable";
+
+}
--- app/src/main/java/in/specialsoft/bloodvault/SplashScreen.java
+++ app/src/main/java/in/specialsoft/bloodvault/SplashScreen.java
@@ -8,17 +8,32 @@ import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import in.specialsoft.bloodvault.DonorDetails.DonorDetails;
+import io.paperdb.Paper;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Paper.init(this);
+
         if (isNetworkConnected())
         {
             Intent intent = new Intent(SplashScreen.this,MainActivity.class);
             startActivity(intent);
             finish();
+
+            String useIdKey = String.valueOf(Paper.book().read(DonorDetails.UserIDKey));
+            if(! useIdKey.equals(""))
+            {
+                this.allowAccess(useIdKey);
+            }
+            else
+            {
+                newUser();
+            }
         }
         else
         {
@@ -37,4 +52,27 @@ public class SplashScreen extends AppCompatActivity {
 
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
+
+    private void newUser() {
+        //to Register
+    }
+
+    private void allowAccess(String useIdKey) {
+        //check user Login
+    }
+
+    private void storeData() {
+
+        Paper.book().write(DonorDetails.UserIDKey,"ID");
+
+        Paper.book().write(DonorDetails.UserNameKey,"Name");
+        Paper.book().write(DonorDetails.UserPhoneKey,"phone");
+        Paper.book().write(DonorDetails.UserAddressKey,"Address");
+        Paper.book().write(DonorDetails.UserCityKey,"city");
+        Paper.book().write(DonorDetails.UserBloodGroupKey,"BloodGroup");
+
+        Paper.book().write(DonorDetails.UserGenderKey,"1/0");
+        Paper.book().write(DonorDetails.UserAvailableKey,"1/0");
+    }
+
 }
